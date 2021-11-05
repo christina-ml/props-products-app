ec47aeca70b75570458d500d0e1861f05322cd46
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-05 02:16:39 -0400

    file setup. Creating a RecommendedProducts component

---
+++ src/Components/RecommendedProducts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class RecommendedProducts extends React.Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div>
+                hello products!
+            </div>
+        )   
+    }
+}
+
+export default RecommendedProducts;
