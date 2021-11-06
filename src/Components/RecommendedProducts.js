71dfdfa3fad4780f298835eb136b231fe0cdffdc
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-06 16:22:03 -0400

    takes entire array, because we mapped shoes to an array of Products Components

--- src/Components/RecommendedProducts.js
+++ src/Components/RecommendedProducts.js
@@ -20,11 +20,12 @@ class RecommendedProducts extends React.Component{
                 <h1>{title}</h1>
                 <ul>
                     {/* <Products shoe={shoes[0]} /> */}
-                    <Products name={shoes[0].name} description={shoes[0].description} />
+                    {/* <Products name={shoes[0].name} description={shoes[0].description} />
                     <Products name={shoes[1].name} description={shoes[1].description} />
                     <Products name={shoes[2].name} description={shoes[2].description} />
                     <Products name={shoes[3].name} description={shoes[3].description} />
-                    <Products name={shoes[4].name} description={shoes[4].description} />
+                    <Products name={shoes[4].name} description={shoes[4].description} /> */}
+                    {recommended}
                 </ul>
             </div>
         )   
