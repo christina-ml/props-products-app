6efe839573b0eca6494b6f723ab9af85339dbe23
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-06 15:29:39 -0400

    how we want our Products.js component to look

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,20 +1,9 @@
 // here we will create a template for a product component!
 
-// function Products( {name, description} ) {
+function Products( {name, description} ) {
     // console.log(props.name)
     // let { name, description } = props.shoe;
      
-//     return (
-//         <div className="product">
-//             <h1>{name}</h1>
-//             <p>{description}</p>
-//         </div>
-//     )
-// }
-
-
-/* How to write functional Component it with an arrow function */
-const Products = ({name, description}) => {
     return (
         <div className="product">
             <h1>{name}</h1>
@@ -22,5 +11,16 @@ const Products = ({name, description}) => {
         </div>
     )
 }
+
+
+/* How to write functional Component it with an arrow function */
+// const Products = ({name, description}) => {
+//     return (
+//         <div className="product">
+//             <h1>{name}</h1>
+//             <p>{description}</p>
+//         </div>
+//     )
+// }
 
 export default Products;
