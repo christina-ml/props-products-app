acdd293f38e4142dfcbd405e46dbf83d5ed10b18
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-06 15:25:27 -0400

    another version of destructuring the props, but in the parameter of the Products function

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,8 +1,8 @@
 // here we will create a template for a product component!
 
-function Products(props) {
-    console.log(props.name)
-    let { name, description } = props.shoe;
+function Products( {name, description} ) {
+    // console.log(props.name)
+    // let { name, description } = props.shoe;
      
     return (
         <div className="product">
